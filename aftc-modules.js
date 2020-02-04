13041ca3c1e75021f4b7df592ea167ef78ff779d
Darcey <Darcey.Lloyd@gmail.com>
2020-02-04 18:14:58 +0000

    v1.1.5
    v1.1.5

--- aftc-modules.js
+++ aftc-modules.js
@@ -1,4 +1,4 @@
-// aftc-modules v1.1.4
+// aftc-modules v1.1.5
 // Author: Darcey@aftc.io
 
 /**
@@ -878,157 +878,6 @@ export function WordLimiter(str, maxWords) {
     return { output: output, remaining: (maxWords - wordCount) };
 }
 
-export function GetRandomBoolean(){
-    return Math.random() >= 0.5;
-}
-export function GetRandomFloat(min, max) {
-    // let r = from + (Math.random()* (to*2));
-    return (Math.random() * (max - min) + min);
-};
-
-export function GetRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-/**
- * @function: getRandomThatsNot(min,max,not)
- * @desc: returns a random int betwen your specified min and max values but never the not value
- * @param min number: the minimum your random number is allowed to go
- * @param max number: the maximum your random number is allowed to go
- * @alias: getRandom
- * @link: https://codepen.io/AllForTheCode/pen/yEBZNq
- */
-export function GetRandomThatsNot(min,max,not){
-    let r = not; let lim = 100; let runs = 0;
-    while (r===not && runs < lim){
-        runs++;
-        r = getRandomInt(min,max);
-    }
-    if (runs>=lim){
-        return false;
-    } else {
-        return r;
-    }
-}
-
-
-
-
-/**
- * @function: getWeightedRandom(odds, iterations)
- * @desc: Get a weighted random based on odds and iterations
- * @param odds array: array of odds
- * @param iterations number: number of iterations to run on each number test
- * @link: https://codepen.io/AllForTheCode/pen/RyvWjZ
- */
-export function GetWeightedRandom(odds, iterations) {
-    if (!odds) {
-        odds = [
-            0.68, // 0
-            0.69, // 1
-            0.698, // 2
-            0.6909, // 3
-            0.68, // 4
-            0.58, // 5
-            0.57, // 6
-            0.56, // 7
-            0.4, // 8
-            0.3, // 9
-        ];
-    }
-    let weights = [];
-    let r = 0;
-    let iMax = 0;
-    let wMax = 0;
-
-    for (let i in odds) {
-        if (!weights[i]) {
-            weights[i] = 0;
-        }
-
-        for (let x = 0; x < iterations; x++) {
-            r = Math.random();
-            //log(r.toFixed(3) + "   " + odds[i].toFixed(3));
-            if (r <= odds[i]) {
-                weights[i] += odds[i];
-            }
-        }
-
-        if (weights[i] > wMax) {
-            wMax = weights[i];
-            iMax = i;
-        }
-    }
-
-    //log(weights);
-    //log("wMax = " + wMax + "   iMax = " + iMax);
-    return iMax;
-};
-
-export function InertiaTo(current,target,amount){
-    if (amount == 1) {
-        return target;
-    }
-    let distToGo = target - current;
-    let delta = current + (distToGo * amount);
-
-    if (Math.abs(distToGo) < 0.01) {
-        distToGo = 0;
-        delta = target;
-    }
-    return delta;
-}
-
-export function IsEven(n) {
-    return n % 2 === 0;
-}
-
-export function IsOdd(n) {
-    return Math.abs(n % 2) === 1;
-}
-export function NormaliseRange(min, max, v) {
-    let range = max - min;
-    let step = 1 / range;
-    let r = (step * (v - min));
-    if (v < min) {
-        r = 0;
-    } else if (v > max) {
-        r = 1;
-    }
-    return r;
-}
-
-export function ParseArrayToFloat(arr) {
-    let converted;
-    for (let i = 0; i < arr.length; i++) {
-        converted = parseFloat(arr[i]);
-        if (isNaN(converted)){
-            arr[i] = 0;
-        } else {
-            arr[i] = converted;
-        }
-    }
-    return arr;
-}
-
-
-export function ParseArrayToInt(arr) {
-    let converted;
-    for (let i = 0; i < arr.length; i++) {
-        converted = parseInt(arr[i]);
-        if (isNaN(converted)){
-            arr[i] = 0;
-        } else {
-            arr[i] = converted;
-        }
-    }
-    return arr;
-}
-
-
-export function RoundTo(v, dec) {
-    return +(Math.round(Number(v + "e+" + dec)) + "e-" + dec);
-}
-
 // TODO: REWORK THIS TO SOMETHING NICE AND SHORT OR USE FETCH
 
 export function XHR() {
@@ -1345,6 +1194,157 @@ export function XHR() {
     }
 }
 
+
+export function GetRandomBoolean(){
+    return Math.random() >= 0.5;
+}
+export function GetRandomFloat(min, max) {
+    // let r = from + (Math.random()* (to*2));
+    return (Math.random() * (max - min) + min);
+};
+
+export function GetRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+/**
+ * @function: getRandomThatsNot(min,max,not)
+ * @desc: returns a random int betwen your specified min and max values but never the not value
+ * @param min number: the minimum your random number is allowed to go
+ * @param max number: the maximum your random number is allowed to go
+ * @alias: getRandom
+ * @link: https://codepen.io/AllForTheCode/pen/yEBZNq
+ */
+export function GetRandomThatsNot(min,max,not){
+    let r = not; let lim = 100; let runs = 0;
+    while (r===not && runs < lim){
+        runs++;
+        r = getRandomInt(min,max);
+    }
+    if (runs>=lim){
+        return false;
+    } else {
+        return r;
+    }
+}
+
+
+
+
+/**
+ * @function: getWeightedRandom(odds, iterations)
+ * @desc: Get a weighted random based on odds and iterations
+ * @param odds array: array of odds
+ * @param iterations number: number of iterations to run on each number test
+ * @link: https://codepen.io/AllForTheCode/pen/RyvWjZ
+ */
+export function GetWeightedRandom(odds, iterations) {
+    if (!odds) {
+        odds = [
+            0.68, // 0
+            0.69, // 1
+            0.698, // 2
+            0.6909, // 3
+            0.68, // 4
+            0.58, // 5
+            0.57, // 6
+            0.56, // 7
+            0.4, // 8
+            0.3, // 9
+        ];
+    }
+    let weights = [];
+    let r = 0;
+    let iMax = 0;
+    let wMax = 0;
+
+    for (let i in odds) {
+        if (!weights[i]) {
+            weights[i] = 0;
+        }
+
+        for (let x = 0; x < iterations; x++) {
+            r = Math.random();
+            //log(r.toFixed(3) + "   " + odds[i].toFixed(3));
+            if (r <= odds[i]) {
+                weights[i] += odds[i];
+            }
+        }
+
+        if (weights[i] > wMax) {
+            wMax = weights[i];
+            iMax = i;
+        }
+    }
+
+    //log(weights);
+    //log("wMax = " + wMax + "   iMax = " + iMax);
+    return iMax;
+};
+
+export function InertiaTo(current,target,amount){
+    if (amount == 1) {
+        return target;
+    }
+    let distToGo = target - current;
+    let delta = current + (distToGo * amount);
+
+    if (Math.abs(distToGo) < 0.01) {
+        distToGo = 0;
+        delta = target;
+    }
+    return delta;
+}
+
+export function IsEven(n) {
+    return n % 2 === 0;
+}
+
+export function IsOdd(n) {
+    return Math.abs(n % 2) === 1;
+}
+export function NormaliseRange(min, max, v) {
+    let range = max - min;
+    let step = 1 / range;
+    let r = (step * (v - min));
+    if (v < min) {
+        r = 0;
+    } else if (v > max) {
+        r = 1;
+    }
+    return r;
+}
+
+export function ParseArrayToFloat(arr) {
+    let converted;
+    for (let i = 0; i < arr.length; i++) {
+        converted = parseFloat(arr[i]);
+        if (isNaN(converted)){
+            arr[i] = 0;
+        } else {
+            arr[i] = converted;
+        }
+    }
+    return arr;
+}
+
+
+export function ParseArrayToInt(arr) {
+    let converted;
+    for (let i = 0; i < arr.length; i++) {
+        converted = parseInt(arr[i]);
+        if (isNaN(converted)){
+            arr[i] = 0;
+        } else {
+            arr[i] = converted;
+        }
+    }
+    return arr;
+}
+
+
+export function RoundTo(v, dec) {
+    return +(Math.round(Number(v + "e+" + dec)) + "e-" + dec);
+}
 
 export function GetGUID() {
     function Amiga() {
